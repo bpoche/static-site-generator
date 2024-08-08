311cfae4c2bf03af2a77c6d2143bbfe8e55f8033
bpoche <brettdp@gmail.com>
2024-08-08 10:13:46 -0500

    feat: added LeafNode and test for leafnodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -4,15 +4,17 @@ class HTMLNode():
     def __init__(self,tag: str = None, value: str = None, children: list = None, props: dict = None):
         self.tag = tag
         self.value = value
-        self.children = children.copy() if children else None
-        self.props = props.copy() if props else None
+        self.children = children.copy() if children else []
+        self.props = props.copy() if props else {}
 
-    def __eq__(self,HTMLNode):
+    def __eq__(self,other):
+        if not isinstance(other,HTMLNode):
+            return False
         if (
-            self.tag == HTMLNode.tag and
-            self.value == HTMLNode.value and
-            self.children == HTMLNode.children and
-            self.props == HTMLNode.props
+            self.tag == other.tag and
+            self.value == other.value and
+            self.children == other.children and
+            self.props == other.props
         ):
             return True
         return False
@@ -24,6 +26,18 @@ class HTMLNode():
         raise NotImplementedError
     
     def props_to_html(self):
+        if not self.props:
+            return ""
         html_str = functools.reduce(lambda acc,item: f'{acc} {item[0]}="{item[1]}"',self.props.items(),"")
         return html_str
     
+class LeafNode(HTMLNode):
+    def __init__(self,tag: str = None, value: str = None, props: dict = None):
+        if not value:
+            raise ValueError("Value is required for all LeafNodes")
+        super().__init__(tag=tag, value=value, children=None, props=props)
+
+    def to_html(self):
+        if not self.tag:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
@@ -33,3 +33,43 @@ class TestHTMLNode(unittest.TestCase):
         actual = node.props_to_html()
         expected = ' href="https://www.google.com" target="_blank"'
         self.assertEqual(expected,actual)
+
+class TestLeafNode(unittest.TestCase):
+    def test_eq(self):
+        node1 = LeafNode(tag="b", value="testing")
+        node2 = LeafNode(tag="b", value="testing")
+        self.assertEqual(node1,node2)
+    
+    def test_neq(self):
+        node1 = LeafNode(tag="b", value="testing")
+        node2 = LeafNode(tag="i", value="testing")
+        self.assertNotEqual(node1,node2)
+
+    # Leaf nodes must have a value, check that an exception is raised if no value is provided
+    def test_no_value(self):
+        with self.assertRaises(ValueError):
+            node = LeafNode(tag="b")
+
+    def test_to_html(self):
+        node = LeafNode("b","testing")
+        actual = node.to_html()
+        expected = "<b>testing</b>"
+        self.assertEqual(expected,actual)
+
+    def test_to_html_props(self):
+        props = {
+            "href": "https://www.google.com",
+            "target": "_blank",
+        }
+        node = LeafNode("a","Google",props)
+        actual = node.to_html()
+        expected = '<a href="https://www.google.com" target="_blank">Google</a>'
+        self.assertEqual(expected,actual)
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None,"testing")
+        actual = node.to_html()
+        expected = "testing"
+        self.assertEqual(expected,actual)
+
+        
