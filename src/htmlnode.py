fa4cdd4a3fab12f44899dbd5135ad7c7ddf703b5
bpoche <brettdp@gmail.com>
2024-08-07 20:45:19 -0500

    feat: added htmlnode.py

---
+++ src/htmlnode.py
@@ -0,0 +1,28 @@
+import functools
+
+class HTMLNode():
+    def __init__(self,tag: str = None, value: str = None, children: list = None, props: dict = None):
+        self.tag = tag
+        self.value = value
+        self.children = children.copy() if children else None
+        self.props = props.copy() if props else None
+
+    def __repr__(self):
+        return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
+
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        html_str = functools.reduce(lambda acc,item: f'{acc} {item[0]}="{item[1]}"',self.props.items(),"")
+        return html_str
+    
+def main():
+    props = {
+    "href": "https://www.google.com", 
+    "target": "_blank",
+}
+    htmlnode = HTMLNode("b","testing",None,props=props)
+    print(repr(htmlnode.props_to_html()))
+
+main()
